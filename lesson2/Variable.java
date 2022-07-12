ae3be7497caccd5ddb860afc1ad6aeb48fa91c67
velevedi <velevedi@gmail.com>
2022-07-12 23:36:09 +0100

    lesson 2 details

---
+++ lesson2/Variable.java
@@ -0,0 +1,11 @@
+/**
+ * evolve to if statement 
+ */
+public class Variable {
+
+    public static void main(String[] args) {
+        String v = "";
+        System.out.println("hello world " + v);
+    }
+
+}
